e740ee9f6a5101b5a7b639907515736db8970ed4
David Pohan <0ronon0@gmail.com>
2019-04-13 23:42:47 +0200

    Move reset card styles into a file

--- src/ResetCard.tsx
+++ src/ResetCard.tsx
@@ -1,25 +1,11 @@
 import * as React from 'react';
 import { Card } from './Card';
+import './ResetCard.css';
 
 export const ResetCard = ({ onReset }: any) => (
     <Card zIndex={0}>
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '100%',
-                justifyContent: 'center',
-            }}
-        >
-            <div
-                style={{
-                    maskImage: 'url(/check.svg)',
-                    backgroundColor: 'green',
-                    width: 100,
-                    height: 100,
-                    alignSelf: 'center',
-                }}
-            />
+        <div className="ResetCard">
+            <div className="check" />
         </div>
         <button onClick={onReset}>Restart</button>
     </Card>
